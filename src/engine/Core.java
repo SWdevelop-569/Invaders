5e8c97b890a1f69349588bf272087e3e22379106
Taeyoon_Kwon <kwonconnor101@gmail.com>
2020-12-02 14:24:45 +0900

    SI-19 master branch 와 conflict해결

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -9,11 +9,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import screen.GameScreen;
-import screen.HighScoreScreen;
-import screen.ScoreScreen;
-import screen.Screen;
-import screen.TitleScreen;
+import screen.*;
 
 import javax.swing.*;
 
@@ -194,11 +190,27 @@ public final class Core {
 				currentScreen = new HighScoreScreen(width, height, FPS);
 				returnCode = frame.setScreen(currentScreen);
 				break;
+			case 5:
+				//mode select
+				currentScreen = new PlayModeScreen(width, height, FPS);
+				LOGGER.info("Starting " + WIDTH + "x" + HEIGHT
+						+ " play mode screen at " + FPS + " fps.");
+				returnCode = frame.setScreen(currentScreen);
+				LOGGER.info("Closing play mode screen");
+				break;
+
+//			case 6:
+			case 7:
+				currentScreen = new TitleScreen(width, height, FPS);
+				LOGGER.info("Starting " + WIDTH + "x" + HEIGHT
+						+ " title screen at " + FPS + " fps.");
+				returnCode = frame.setScreen(currentScreen);
+				LOGGER.info("Closing title screen");
+				break;
 
 			default:
 				break;
 			}
-
 		} while (returnCode != 0);
 
 		fileHandler.flush();
